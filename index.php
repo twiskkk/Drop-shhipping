261c07627b221037e0f65891d5fae8de9c580427
twiskkk <105844209+twiskkk@users.noreply.github.com>
2022-06-21 17:30:54 +0200

    v1 site menu + forme

---
+++ index.php
@@ -0,0 +1,332 @@
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="GetTemplate.com">
+    <title>Hostips</title>
+    <link rel="shortcut icon" href="assets/images/gt_favicon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:200,400,600|Open+Sans:300,400,700">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="assets/css/styles.css"> 
+
+</head>
+
+<body class="page-home">
+
+    <nav class="navbar navbar-dual navbar-inverse navbar-fixed-top headroom ontop-now">
+        <div class="container">
+            <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span
+                        class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                <a class="navbar-brand" href="index.php">
+                    <img src="assets/images/logo.webp" alt="logo" height="56" width="72">
+                    <img src="assets/images/logo.webp" alt="Progressus HTML5 template" class="secondary" height="56" width="72">
+                </a>
+
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav pull-right">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="tendency.php">Tendency</a></li>
+                    <li><a href="new.php">New</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a class="btn btn-rounded" href="signin.html">SIGN IN / SIGN UP</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Index -->
+    <header class="head head-default">
+        <div class="container">
+            <h1 class="lead text-center">Hostips</h1>
+            <p class="tagline text-center">welcome to Hostips, the site for good deals<a
+                    href="https://www.gettemplate.com/?utm_source=progressus&amp;utm_medium=template&amp;utm_campaign=progressus">GetTemplate</a>
+            </p>
+            <p class="text-center">
+                <a class="btn btn-default btn-lg"
+                    href="tendency.php">Tendency</a>
+                <a class="btn btn-action btn-lg"
+                    href="shop.php">BUY
+                    NOW FOR $19</a>
+            </p>
+        </div>
+    </header>
+
+
+    <section class="section section-intro">
+        <div class="container">
+            <h2 class="text-center">Trending items</h2>
+            <p class="text-center text-muted">
+            here are the favorite items of our customers.
+            </p>
+        </div>
+    </section>
+
+
+    <section class="section jumbotron">
+        <div class="container">
+            <h3 class="title text-center">
+                REASONS TO USE THIS TEMPLATE<br>
+                <small>Progressus PRO has just the right feature set to help you get beautiful, maintainable website
+                    fast.</small>
+            </h3>
+            <div class="row topspace-2x">
+                <figure class="col-md-3 col-sm-6 bottomspace-xs text-center">
+                    <i class="fa fa-cogs fa-4x color-accent"></i>
+                    <h4>Bootstrap-powered</h4>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aliquid
+                        adipisci aspernatur. Soluta quisquam dignissimos earum quasi voluptate. Amet, dignissimos,
+                        tenetur vitae dolor quam iusto assumenda hic reprehenderit?</p>
+                </figure>
+                <figure class="col-md-3 col-sm-6 bottomspace-xs text-center">
+                    <i class="fa fa-paper-plane-o fa-4x color-accent"></i>
+                    <h4>Fat-free</h4>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores,
+                        commodi, sequi quis ad fugit omnis cumque a libero error nesciunt molestiae repellat quos
+                        perferendis numquam quibusdam rerum repellendus laboriosam reprehenderit! </p>
+                </figure>
+                <figure class="col-md-3 col-sm-6 bottomspace-xs text-center">
+                    <i class="fa fa-cc fa-4x color-accent"></i>
+                    <h4>Creative Commons</h4>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, vitae,
+                        perferendis, perspiciatis nobis voluptate quod illum soluta minima ipsam ratione quia numquam
+                        eveniet eum reprehenderit dolorem dicta nesciunt corporis?</p>
+                </figure>
+                <figure class="col-md-3 col-sm-6 bottomspace-xs text-center">
+                    <i class="fa fa-life-ring fa-4x color-accent"></i>
+                    <h4>Author's support</h4>
+                    <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, excepturi,
+                        maiores, dolorem quasi reprehenderit illo accusamus nulla minima repudiandae quas ducimus
+                        reiciendis odio sequi atque temporibus facere corporis eos expedita? </p>
+                </figure>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="section section-faq">
+        <div class="container">
+            <h2 class="text-center title">Frequently Asked Questions</h2>
+            <br>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h4><b>Which code editor would you recommend?</b></h4>
+                    <p>I'd highly recommend you <a href="https://www.sublimetext.com/">Sublime Text</a> - a free to try
+                        text editor which I'm using daily. Awesome tool!</p>
+                </div>
+                <div class="col-sm-6">
+                    <h4><b>Nice header. Where do I find more images like that one?</b></h4>
+                    <p>
+                        Well, there are thousands of stock art galleries, but personally,
+                        I prefer to use photos from these sites: <a href="https://unsplash.com">Unsplash.com</a>
+                        and <a href="https://www.flickr.com/creativecommons/by-2.0/tags/">Flickr - Creative Commons</a>
+                    </p>
+                </div>
+            </div>
+            <div class="row topspace">
+                <div class="col-sm-6">
+                    <h4><b>Can I use it to build a site for my client?</b></h4>
+                    <p>
+                        Yes, you can. You may use this template for any purpose, just don't forget about the <a
+                            href="https://creativecommons.org/licenses/by/3.0/">license</a>,
+                        which says: "You must give appropriate credit", i.e. you must provide the name of the creator
+                        and a link to the original template in your work.
+                    </p>
+                </div>
+                <div class="col-sm-6">
+                    <h4><b>Can you customize this template for me?</b></h4>
+                    <p>Yes, I can. Please drop me a line to sergey-at-pozhilov.com and describe your needs in details.
+                        Please note, my services are not cheap.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section section-testimonials jumbotron">
+        <div class="container">
+            <h2 class="title text-center topspace">What people are saying about us?</h2>
+            <div id="testimonials-carousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#testimonials-carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#testimonials-carousel" data-slide-to="1"></li>
+                    <li data-target="#testimonials-carousel" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner">
+                    <div class="row item active">
+                        <div class="col-sm-3 profile">
+                            <img src="assets/images/people/1.jpg" alt="" />
+                        </div>
+                        <div class="col-sm-9 content">
+                            <blockquote>
+                                <i class="fa fa-quote-left"></i>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam deserunt
+                                    accusamus soluta maiores alias, aliquid ipsum nobis laudantium qui, eos architecto
+                                    accusantium doloremque dolorem quia asperiores voluptate pariatur. Laboriosam,
+                                    ullam.</p>
+                            </blockquote>
+                            <p class="source">Mario Hayes
+                                <br /><span class="title">Co-Founder, Superawesome Inc</span>
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="row item">
+                        <div class="col-sm-3 profile">
+                            <img src="assets/images/people/2.jpg" alt="" />
+                        </div>
+                        <div class="col-sm-9 content">
+                            <blockquote>
+                                <i class="fa fa-quote-left"></i>
+                                <p>Ipsa nemo, minus perspiciatis harum at, repudiandae quae aliquam quo? Ullam laborum
+                                    unde corporis eos ipsa esse necessitatibus in natus atque labore delectus, aperiam
+                                    perferendis, assumenda iusto qui cum dolor!</p>
+                            </blockquote>
+                            <p class="source">Erin Mendoza
+                                <br /><span class="title">Entrepreneur, GT corp</span>
+                            </p>
+                        </div>
+
+                    </div>
+
+                    <div class="row item">
+                        <div class="col-sm-3 profile">
+                            <img src="assets/images/people/3.jpg" alt="" />
+                        </div>
+                        <div class="col-sm-9 content">
+                            <blockquote>
+                                <i class="fa fa-quote-left"></i>
+                                <p>Voluptatibus laboriosam pariatur doloribus repudiandae blanditiis cum quibusdam
+                                    similique nisi nemo labore eos sed quasi quia, assumenda, ratione sit provident
+                                    beatae hic eius velit neque magni distinctio ab quaerat ipsa!</p>
+                            </blockquote>
+                            <p class="source">Dwight Ray
+                                <br /><span class="title">CTO, Drave</span>
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <section class="section section-cta">
+        <div class="container">
+            <h2>Get started
+                <span>Start your free, 30 day trial today!</span>
+            </h2>
+            <div class="row topspace">
+                <div class="col-sm-9">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat velit voluptatem quae tempora
+                        blanditiis sunt fugit facere praesentium vero, est natus, id accusantium ab animi voluptatibus
+                        magnam laborum ad nobis nam assumenda dolores quod cumque!</p>
+                </div>
+                <div class="col-sm-3"><a href="" class="btn btn-block btn-action">Start free trial</a></div>
+            </div>
+        </div>
+    </section>
+    <footer id="footer" class="clearfix">
+        <div class="footer1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 widget">
+                        <h3 class="widget-title">Contact</h3>
+                        <div class="widget-body">
+                            <p>+234 23 9873237<br>
+                                <a href="/cdn-cgi/l/email-protection#6043"><span class="__cf_email__"
+                                        data-cfemail="91e2fefcf4bff4fcf0f8fdd1e2fefcf4e6f9f4e3f4bff2fefc">[email&#160;protected]</span></a><br>
+                                <br>
+                                234 Hidden Pond Road, Ashland City, TN 37015
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 widget">
+                        <h3 class="widget-title">Follow us</h3>
+                        <div class="widget-body">
+                            <p class="follow-me-icons">
+                                <a href=""><i class="fa fa-twitter fa-2"></i></a>
+                                <a href=""><i class="fa fa-dribbble fa-2"></i></a>
+                                <a href=""><i class="fa fa-github fa-2"></i></a>
+                                <a href=""><i class="fa fa-facebook fa-2"></i></a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 widget">
+                        <h3 class="widget-title">Text widget</h3>
+                        <div class="widget-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam
+                                architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque
+                                voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad
+                                id expedita cupiditate repellendus possimus unde?</p>
+                            <p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat
+                                provident assumenda labore soluta minima alias temporibus facere distinctio quas
+                                adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate
+                                reprehenderit architecto sint libero illo et hic.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 widget">
+                        <div class="widget-body">
+                            <p class="simplenav">
+                                <a href="#">Home</a> |
+                                <a href="about.html">About</a> |
+                                <a href="sidebar-right.html">Sidebar</a> |
+                                <a href="contact.html">Contact</a> |
+                                <b><a href="signup.html">Sign up</a></b>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 widget">
+                        <div class="widget-body">
+                            <p class="text-right">
+                                Copyright &copy; 2014, Your name. Design: <a href="https://gettemplate.com/"
+                                    rel="designer">GetTemplate</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-48366846-1', 'gettemplate.com');
+    ga('send', 'pageview');
+    </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap-hover-dropdown.min.js"></script>
+    <script src="assets/js/headroom.min.js"></script>
+    <script src="assets/js/jQuery.headroom.min.js"></script>
+    <script src="assets/js/template.js"></script>
+</body>
+
+</html>
